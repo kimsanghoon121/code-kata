11abee849cda461267c9142b44ba9676260772bc
kimsanghoon121 <na773014@gmail.com>
2026-01-07 08:21:07 +0900

    Add files via upload

---
+++ 260107 codekata.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double sum = 0;
+    for (double num : arr) {
+        sum += num;
+    }
+
+    double answer = sum / arr.size();
+    return answer;
+}
