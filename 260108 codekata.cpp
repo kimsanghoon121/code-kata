c32d77811615ee43a4f7b9358f0765347b04992b
kimsanghoon121 <na773014@gmail.com>
2026-01-08 08:51:58 +0900

    Add files via upload

---
+++ 260108 codekata.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    if (n < 0 || n>3000) {
+        return 0;
+    }
+    else {
+        for (int i = 1; i <= n; ++i) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+    }
+    return answer;
+}
