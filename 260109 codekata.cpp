a21fd3b91161261cb8da0459952d53068c233c98
kimsanghoon121 <na773014@gmail.com>
2026-01-09 08:23:45 +0900

    Add files via upload

---
+++ 260109 codekata.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 2;
+    while ((n - 1) % answer != 0) {
+        answer++;
+    }
+    return answer;
+}
