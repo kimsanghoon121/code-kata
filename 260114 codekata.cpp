99d6b82a81321c0d6961a8fc8fcbaf2484415b10
kimsanghoon121 <na773014@gmail.com>
2026-01-14 09:35:17 +0900

    Add files via upload

---
+++ 260114 codekata.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    int pm = 1;
+
+    if (s[0] == '-') {
+        pm = -1;
+    }
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            answer *= 10;
+            answer += s[i] - '0';
+        }
+    }
+    answer *= pm;
+    return answer;
+}
