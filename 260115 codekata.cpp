c3f6c8e3fe07731806b3b59d852f1a59bee2af6f
kimsanghoon121 <na773014@gmail.com>
2026-01-15 09:13:23 +0900

    Add files via upload

---
+++ 260115 codekata.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    for (long long i = 1; i <= sqrt(n); ++i) {
+        if (n == i * i)
+        {
+            return (i + 1) * (i + 1);
+        }
+    }
+    return -1;
+}
