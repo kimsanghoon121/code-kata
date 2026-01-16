1464ddb77ca8f91d5706991a9b46641b13435e3e
kimsanghoon121 <na773014@gmail.com>
2026-01-16 09:18:37 +0900

    Add files via upload

---
+++ 260116 codekata.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    string sortnum = to_string(n);
+    sort(sortnum.rbegin(), sortnum.rend());
+    long long answer = stoll(sortnum);
+    return answer;
+}
