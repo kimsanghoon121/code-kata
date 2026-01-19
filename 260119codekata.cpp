7bb242ba1f8cbc6ad22656ef7691ee699674efcc
kimsanghoon121 <na773014@gmail.com>
2026-01-19 09:08:23 +0900

    Add files via upload

---
+++ 260119codekata.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int divide = 0;
+    int value = x;
+    while (x != 0)
+    {
+        divide += x % 10;
+        x /= 10;
+    }
+    if (value % divide == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
