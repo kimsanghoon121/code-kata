755e79e7b8c2c1fd844ad29e94df44ecc4462ca6
kimsanghoon121 <na773014@gmail.com>
2026-01-22 09:52:50 +0900

    Add files via upload

---
+++ 260122 codekata.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    auto it = find(seoul.begin(), seoul.end(), "Kim");
+    string answer = "김서방은 " + to_string(distance(seoul.begin(), it)) + "에 있다";
+    return answer;
+}
