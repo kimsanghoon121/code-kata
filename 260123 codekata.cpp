c43484b39dd0d27c3a5924fdf5946b2580bd9633
kimsanghoon121 <na773014@gmail.com>
2026-01-23 09:04:55 +0900

    Add files via upload

---
+++ 260123 codekata.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for (int a : arr)
+    {
+        if (a % divisor == 0)
+        {
+            answer.push_back(a);
+        }
+    }
+    if (answer.size() == 0) {
+        answer.push_back(-1);
+        return answer;
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
