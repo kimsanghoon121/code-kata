d8cd0efb7283ec9ec04439a4f601cd49efbf9c78
kimsanghoon121 <na773014@gmail.com>
2026-01-28 08:27:03 +0900

    Add files via upload

---
+++ 260128 codekata.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 45;
+    int sum = 0;
+    for (int a : numbers)
+    {
+        sum += a;
+    }
+    answer -= sum;
+    return answer;
+}
