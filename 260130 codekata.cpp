90468444059fe032608f74b655c6079173b52d58
kimsanghoon121 <na773014@gmail.com>
2026-01-30 08:54:27 +0900

    Add files via upload

---
+++ 260130 codekata.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int index = s.size();
+    if (index % 2 == 0)
+    {
+        answer += s[index / 2 - 1];
+        answer += s[index / 2];
+    }
+    if (index % 2 != 0)
+    {
+        answer = s[(index / 2)];
+    }
+    return answer;
+}
