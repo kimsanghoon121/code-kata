3cb2dc679b10bdf56f3dbf46bb2c20665489518b
kimsanghoon121 <na773014@gmail.com>
2026-02-09 08:23:22 +0900

    Add files via upload

---
+++ 260209 codekata.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    if (s.length() == 4 || s.length() == 6)
+    {
+        for (auto ch : s)
+        {
+            if (!isdigit(ch)) return false;
+        }
+        return true;
+    }
+    else return false;
+}
