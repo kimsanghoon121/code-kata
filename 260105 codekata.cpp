87053ee5890baf01d9e7edb75e52e6403a454454
kimsanghoon121 <na773014@gmail.com>
2026-02-09 20:54:03 +0900

    폴더 구조 정리
    폴더 구조 정리

---
+++ 260105 codekata.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double sum = 0;
+    for (double num : numbers) {
+        sum += num;
+    }
+
+    double answer = sum / numbers.size();
+    return answer;
+}
