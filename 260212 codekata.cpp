3b700e4bd3d51525bb481ad93d366902a641e93c
kimsanghoon121 <na773014@gmail.com>
2026-02-12 09:34:55 +0900

    Create 260212 codekata.cpp

---
+++ 260212 codekata.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    int lcm = m * n;
+    while (m != 0) {
+        int temp = m;
+        m = n % m;
+        n = temp;
+    }
+    int gcd = n;
+    lcm /= n;
+    answer.push_back(gcd);
+    answer.push_back(lcm);
+    return answer;
+}
