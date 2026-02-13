c2b8c0438a484d168a417ad5430a1819b56404f2
kimsanghoon121 <na773014@gmail.com>
2026-02-13 10:00:10 +0900

    Create 260213 codekata.cpp

---
+++ 260213 codekata.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> a;
+    while (n != 0)
+    {
+        a.push_back(n % 3);
+        n /= 3;
+    }
+    long long power = 1;
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
+        answer += a[i] * power;
+        power *= 3;
+    }
+
+    return answer;
+}
