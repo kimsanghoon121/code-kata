7eec671fe8396d2231e766d49229cece01e4e512
kimsanghoon121 <na773014@gmail.com>
2026-02-19 09:55:35 +0900

    Create 260219 codekata.cpp

---
+++ 260219 codekata.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int index = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            answer += ' ';
+            index = 0;
+        }
+        else {
+            if (index % 2 == 0) {
+                if (s[i] >= 'a' && s[i] <= 'z') {
+                    answer += (s[i] - 32);
+                }
+                else answer += s[i];
+            }
+            else {
+                if (s[i] >= 'A' && s[i] <= 'Z') {
+                    answer += (s[i] + 32);
+                }
+                else answer += s[i];
+            }
+            index++;
+        }
+    }
+    return answer;
+}
