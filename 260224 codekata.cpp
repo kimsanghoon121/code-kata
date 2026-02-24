f91c40ef77fbf567ac989f1b46cb1b1e70fb207c
kimsanghoon121 <na773014@gmail.com>
2026-02-24 09:26:33 +0900

    Create 260224 codekata.cpp

---
+++ 260224 codekata.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    int tlen = t.length();
+    int plen = p.length();
+    for (int i = 0; i <= tlen - plen; ++i)
+    {
+        string s = "";
+        for (int j = i; j < i + plen; ++j)
+        {
+            s += t[j];
+        }
+        if (stoll(s) <= stoll(p))
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
