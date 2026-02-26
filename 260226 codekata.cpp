8f11142219246684fdaa07a4aa871c0d6446bcc9
kimsanghoon121 <na773014@gmail.com>
2026-02-26 09:11:23 +0900

    Create 260226 codekata.cpp

---
+++ 260226 codekata.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    int width = 0;
+    int height = 0;
+    for (auto& vec : sizes)
+    {
+        sort(vec.begin(), vec.end());
+    }
+    for (int i = 0; i < sizes.size(); ++i)
+    {
+        if (sizes[i][0] >= width) {
+            width = sizes[i][0];
+        }
+        if (sizes[i][1] >= height) {
+            height = sizes[i][1];
+        }
+    }
+    answer = width * height;
+    return answer;
+}
