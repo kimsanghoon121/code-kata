e9d3d4da8b185cadb520676b6884387b3158cd1e
kimsanghoon121 <na773014@gmail.com>
2026-03-03 09:15:17 +0900

    Create 260303 codekata.cpp

---
+++ 260303 codekata.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    for (auto chars : s)
+    {
+        if (isspace(chars))
+        {
+            answer += chars;
+            continue;
+        }
+
+        for (int i = 1; i <= n; ++i)
+        {
+            if (chars == 'z' || chars == 'Z')
+            {
+                chars -= 25;
+            }
+            else {
+                chars++;
+            }
+        }
+        answer += chars;
+    }
+
+    return answer;
+}
