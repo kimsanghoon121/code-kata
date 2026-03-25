af2f41742e5a6052b573d63ae391c9cb4aa543f6
kimsanghoon121 <na773014@gmail.com>
2026-03-25 09:54:34 +0900

    Create 260325 codekata.cpp

---
+++ 260325 codekata.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        for (int j = 0; j < numbers.size(); ++j)
+        {
+            if (i == j) continue;
+
+            int sol = numbers[i] + numbers[j];
+
+            if (find(answer.begin(), answer.end(), sol) != answer.end())
+            {
+                continue;
+            }
+            answer.push_back(sol);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
